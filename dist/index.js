9b3c742e839588b848e947281633c910dd9b9bd5
shuai.li12 <shuai.li12@bianlifeng.com>
2018-06-17 15:01:27 +0800

    refactor: 1.refactor to class

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
  * @file index.js
@@ -14,176 +16,194 @@ var validUrl = require('valid-url');
 var parseUrl = require('url').parse;
 
 var _require = require('../lib/utils'),
-    log = _require.log;
+    log = _require.log,
+    getTypeOf = _require.getTypeOf;
 
-var rootPath = process.cwd();
 var proxy = httpProxy.createProxyServer({});
-var paramList = process.argv;
 
-var configFile = path.resolve(rootPath, 'wproxy.js');
-if (!fs.existsSync(configFile)) {
-  log.error('No wproxy.js folder found in ' + rootPath);
-  process.exit(-1);
-}
+var BeeProxy = function () {
+  function BeeProxy() {
+    _classCallCheck(this, BeeProxy);
 
-// 获取到所有需要替换的参数
-var replaceParamMap = getReplaceParamMap(paramList);
-var config = requireUncached(configFile);
+    this.rootPath = process.cwd();
+    this.argvList = process.argv;
+    this.configFilePath = path.resolve(this.rootPath, 'wproxy.js');
 
-var envList = Object.keys(config).filter(function (item) {
-  return item !== 'globalRules';
-});
+    this.config = {};
+    this.currentRuleList = [];
+    this.replaceVariableMap = {};
 
-if (envList.length === 0) {
-  log.error('No available env found in wproxy.js, please retry a later!!!');
-  process.exit(-1);
-}
-var currentEnv = getCurrentEnv(paramList, envList);
-if (!currentEnv) {
-  log.warn('You have not set a  backend env params ,use ' + envList[0] + ' as default env');
-  currentEnv = envList[0];
-}
-
-var targetParam = '';
-if (currentEnv.split('=').length > 1) {
-  var _currentEnv$split = currentEnv.split('=');
-
-  var _currentEnv$split2 = _slicedToArray(_currentEnv$split, 2);
-
-  currentEnv = _currentEnv$split2[0];
-  targetParam = _currentEnv$split2[1];
-}
-
-var currentRuleList = config[currentEnv];
-
-// 将responder中的变量替换掉
-var replaceKeys = Object.keys(replaceParamMap);
-currentRuleList = currentRuleList.map(function (rule) {
-  if (getTypeOf(rule.responder) !== 'string') return rule;
-  replaceKeys.forEach(function (key) {
-    var value = replaceParamMap[key];
-    var regexp = '#\\{' + key + '\\}';
-    rule.responder = rule.responder.replace(new RegExp(regexp, 'g'), value);
-  });
-  return rule;
-});
-
-var globleRuleList = config.globalRules || [];
-
-currentRuleList = globleRuleList.concat(currentRuleList);
-
-function getTypeOf(obj) {
-  if (/^\[object\s(.*)\]/.test(Object.prototype.toString.call(obj))) {
-    return RegExp.$1.toLowerCase();
+    this._checkConfigFileExist();
+    this._readConfigFileContent();
+    this._getReplaceParamMap();
+    this._getCurrentEnvAndRuleList();
+    this._replaceVariableInResponder();
+    this._addGlobleRule();
   }
-  return '';
-}
 
-function getReplaceParamMap(paramList) {
-  var paramMap = {};
-  paramList.forEach(function (item) {
-    var paramList = item.split('=');
-    if (paramList.length === 2) {
-      paramMap[paramList[0]] = paramList[1];
+  _createClass(BeeProxy, [{
+    key: '_checkConfigFileExist',
+    value: function _checkConfigFileExist() {
+      if (!fs.existsSync(this.configFilePath)) {
+        log.error('No wproxy.js folder found in ' + this.rootPath);
+        process.exit(-1);
+      }
     }
-  });
-  return paramMap;
-}
-
-// 给一个list返回
-function getCurrentEnv() {
-  var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-  var envs = arguments[1];
-
-  var result = '';
-  args.forEach(function (item) {
-    if (!item) return;
-    if (envs.includes(item.split('=')[0])) {
-      result = item;
+  }, {
+    key: '_readConfigFileContent',
+    value: function _readConfigFileContent() {
+      this.config = requireUncached(this.configFilePath);
     }
-  });
-  return result;
-}
-
-function getRule(currentRuleList, req, replaceParamMap) {
-  var currentRule = '';
-  currentRuleList.forEach(function (item) {
-    if (item.pattern.test(req.url)) {
-      currentRule = item.responder;
+  }, {
+    key: '_getReplaceParamMap',
+    value: function _getReplaceParamMap() {
+      var paramMap = {};
+      this.argvList.forEach(function (item) {
+        var paramList = item.split('=');
+        if (paramList.length === 2) {
+          paramMap[paramList[0]] = paramList[1];
+        }
+      });
+      this.replaceVariableMap = paramMap;
     }
-  });
-  return currentRule;
-}
-
-function getExecuterType(executer) {
-  var executerType = '';
-  var type = getTypeOf(executer);
-  if (type === 'string') {
-    // 判断是文件夹还是域名
-    if (validUrl.isWebUri(executer)) {
-      executerType = 'web'; //  http or https
+  }, {
+    key: '_getCurrentEnvAndRuleList',
+    value: function _getCurrentEnvAndRuleList() {
+      var envList = Object.keys(this.config).filter(function (item) {
+        return item !== 'globalRules';
+      });
+      if (envList.length === 0) {
+        log.error('No available env found in wproxy.js, please retry a later!!!');
+        process.exit(-1);
+      }
+      var currentEnv = this._getCurrentEnv(envList);
+      if (!currentEnv) {
+        log.warn('You have not set a  backend env params ,use ' + envList[0] + ' as default env');
+        currentEnv = envList[0];
+      }
+      this.currentRuleList = this.config[currentEnv];
     }
-    if (fs.existsSync(path.join(rootPath, executer))) {
-      executerType = 'dir'; //   本地目录
+  }, {
+    key: '_getCurrentEnv',
+    value: function _getCurrentEnv(envs) {
+      var result = '';
+      this.argvList.forEach(function (item) {
+        if (!item) return;
+        var key = item.split('=')[0];
+        if (envs.includes(key)) {
+          result = key;
+        }
+      });
+      return result;
     }
-  } else if (type === 'function') {
-    executerType = 'func';
-  }
-  return executerType;
-}
+  }, {
+    key: '_replaceVariableInResponder',
+    value: function _replaceVariableInResponder() {
+      var _this = this;
 
-function executeHttpResponder(rule, req, res) {
-  log.info(req.url, '----->', rule);
-  proxy.web(req, res, {
-    target: rule,
-    changeOrigin: true
-  });
-}
+      var replaceKeys = Object.keys(this.replaceVariableMap);
+      this.currentRuleList = this.currentRuleList.map(function (rule) {
+        if (getTypeOf(rule.responder) !== 'string') return rule;
+        replaceKeys.forEach(function (key) {
+          var value = _this.replaceVariableMap[key];
+          var regexp = '#\\{' + key + '\\}';
+          rule.responder = rule.responder.replace(new RegExp(regexp, 'g'), value);
+        });
+        return rule;
+      });
+    }
+  }, {
+    key: '_addGlobleRule',
+    value: function _addGlobleRule() {
+      var globleRuleList = this.config.globalRules || [];
+      this.currentRuleList = globleRuleList.concat(this.currentRuleList);
+    }
+  }, {
+    key: 'getResponder',
+    value: function getResponder(req) {
+      var currentRes = '';
+      this.currentRuleList.forEach(function (item) {
+        if (item.pattern.test(req.url)) {
+          currentRes = item.responder;
+        }
+      });
+      return currentRes;
+    }
+  }, {
+    key: '_getResponderType',
+    value: function _getResponderType(responderProcessor) {
+      var responderType = '';
+      var type = getTypeOf(responderProcessor);
+      if (type === 'string') {
+        if (validUrl.isWebUri(responderProcessor)) {
+          responderType = 'web'; //  http or https
+        }
+        if (fs.existsSync(path.join(this.rootPath, responderProcessor))) {
+          responderType = 'dir'; //  local folder
+        }
+      } else if (type === 'function') {
+        responderType = 'func';
+      }
+      return responderType;
+    }
+  }, {
+    key: '_executeHttpResponder',
+    value: function _executeHttpResponder(rule, req, res) {
+      log.info(req.url, '----->', rule);
+      proxy.web(req, res, {
+        target: rule,
+        changeOrigin: true
+      });
+    }
+  }, {
+    key: '_executeDirResponder',
+    value: function _executeDirResponder(mockPath, req, res) {
+      var url = parseUrl(req.url).pathname;
+      var jsonPath = path.join(this.rootPath, mockPath, url + '.json');
+      log.info(req.url, '----->', jsonPath);
+      var json = fs.readFileSync(jsonPath);
+      res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+      res.end(json);
+    }
+  }, {
+    key: '_executeFuncResponder',
+    value: function _executeFuncResponder(func, req, res) {
+      var json = JSON.stringify(func(req));
+      res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+      res.end(json);
+    }
+  }, {
+    key: 'executeResponderProcessor',
+    value: function executeResponderProcessor(responderProcessor, req, res) {
 
-function executeDirResponder(mockPath, req, res) {
-  var url = parseUrl(req.url).pathname;
-  var jsonPath = path.join(rootPath, mockPath, url + '.json');
-  log.info(req.url, '----->', jsonPath);
-  var json = fs.readFileSync(jsonPath);
-  res.setHeader('Content-Type', 'application/json;charset=UTF-8');
-  res.end(json);
-}
+      // check type ,then dispatch corresponding processor
+      var responderType = this._getResponderType(responderProcessor);
+      if (!responderType) {
+        log.error(rule + ' config error, please retry after fix it');
+        process.exit(-1);
+      }
+      switch (responderType) {
+        case 'web':
+          this._executeHttpResponder(responderProcessor, req, res);
+          break;
+        case 'dir':
+          this._executeDirResponder(responderProcessor, req, res);
+          break;
+        case 'func':
+          this._executeFuncResponder(responderProcessor, req, res);
+          break;
+      }
+    }
+  }]);
 
-function executeFuncResponder(func, req, res) {
-  var json = JSON.stringify(func(req));
-  res.setHeader('Content-Type', 'application/json;charset=UTF-8');
-  res.end(json);
-}
+  return BeeProxy;
+}();
 
-function executeRule(rule, req, res) {
-
-  // 判断类型 根据类型分别调用不同的方法
-  // 检查类型 分别给不同的路由函数进行处理
-  var executerType = getExecuterType(rule);
-  if (!executerType) {
-    log.error(rule + ' config error, please retry after fix it');
-    process.exit(-1);
-  }
-  switch (executerType) {
-    case 'web':
-      executeHttpResponder(rule, req, res);
-      break;
-    case 'dir':
-      executeDirResponder(rule, req, res);
-      break;
-    case 'func':
-      executeFuncResponder(rule, req, res);
-      break;
-  }
-}
-
+var beeProxy = new BeeProxy();
 module.exports = function (req, res, next) {
-
-  // 获取当前url适配的url,执行
-  var rule = getRule(currentRuleList, req, replaceParamMap);
-
-  if (rule) {
-    executeRule(rule, req, res);
+  var responderProcessor = beeProxy.getResponder(req);
+  if (responderProcessor) {
+    beeProxy.executeResponderProcessor(responderProcessor, req, res);
     return;
   }
   next && next();
--- src/index.js
+++ src/index.js
@@ -8,170 +8,171 @@ const requireUncached = require('require-uncached');
 const httpProxy = require('http-proxy');
 const validUrl = require('valid-url');
 const parseUrl = require('url').parse;
-const {log} = require('../lib/utils');
-
-const rootPath = process.cwd();
+const {log, getTypeOf} = require('../lib/utils');
 const proxy = httpProxy.createProxyServer({});
-const paramList = process.argv;
 
-const configFile = path.resolve(rootPath, 'wproxy.js');
-if (!fs.existsSync(configFile)) {
-  log.error(`No wproxy.js folder found in ${rootPath}`);
-  process.exit(-1);
-}
+class BeeProxy {
+  constructor() {
+    this.rootPath = process.cwd();
+    this.argvList = process.argv;
+    this.configFilePath = path.resolve(this.rootPath, 'wproxy.js');
 
-// 获取到所有需要替换的参数
-const replaceParamMap = getReplaceParamMap(paramList);
-let config = requireUncached(configFile);
+    this.config = {};
+    this.currentRuleList = [];
+    this.replaceVariableMap = {};
 
-const envList = Object.keys(config).filter((item) => item !== 'globalRules');
-
-if (envList.length === 0) {
-  log.error(`No available env found in wproxy.js, please retry a later!!!`);
-  process.exit(-1);
-}
-let currentEnv = getCurrentEnv(paramList, envList);
-if (!currentEnv) {
-  log.warn(`You have not set a  backend env params ,use ${envList[0]} as default env`);
-  currentEnv = envList[0];
-}
-
-
-let targetParam = '';
-if (currentEnv.split('=').length > 1) {
-  [currentEnv, targetParam] = currentEnv.split('=');
-}
-
-let currentRuleList = config[currentEnv];
-
-// 将responder中的变量替换掉
-const replaceKeys = Object.keys(replaceParamMap);
-currentRuleList = currentRuleList.map((rule) => {
-  if (getTypeOf(rule.responder) !== 'string') return rule;
-  replaceKeys.forEach((key) => {
-    const value = replaceParamMap[key];
-    const regexp = `#\\{${key}\\}`;
-    rule.responder = rule.responder.replace(new RegExp(regexp, 'g'), value);
-  });
-  return rule;
-});
-
-const globleRuleList = config.globalRules || [];
-
-currentRuleList = globleRuleList.concat(currentRuleList);
-
-function getTypeOf(obj) {
-  if (/^\[object\s(.*)\]/.test(Object.prototype.toString.call(obj))) {
-    return RegExp.$1.toLowerCase();
+    this._checkConfigFileExist();
+    this._readConfigFileContent();
+    this._getReplaceParamMap();
+    this._getCurrentEnvAndRuleList();
+    this._replaceVariableInResponder();
+    this._addGlobleRule();
   }
-  return ''
-}
 
-
-function getReplaceParamMap(paramList) {
-  let paramMap = {};
-  paramList.forEach((item) => {
-    const paramList = item.split('=');
-    if (paramList.length === 2) {
-      paramMap[paramList[0]] = paramList[1];
-    }
-  });
-  return paramMap;
-}
-
-// 给一个list返回
-function getCurrentEnv(args = [], envs) {
-  let result = '';
-  args.forEach((item) => {
-    if (!item) return;
-    if (envs.includes(item.split('=')[0])) {
-      result = item;
-    }
-  });
-  return result;
-}
-
-function getRule(currentRuleList, req, replaceParamMap) {
-  let currentRule = '';
-  currentRuleList.forEach((item) => {
-    if (item.pattern.test(req.url)) {
-      currentRule = item.responder;
-    }
-  });
-  return currentRule;
-}
-
-function getExecuterType(executer) {
-  let executerType = '';
-  const type = getTypeOf(executer);
-  if (type === 'string') {
-    // 判断是文件夹还是域名
-    if (validUrl.isWebUri(executer)) {
-      executerType = 'web'; //  http or https
-    }
-    if (fs.existsSync(path.join(rootPath, executer))) {
-      executerType = 'dir'; //   本地目录
+  _checkConfigFileExist() {
+    if (!fs.existsSync(this.configFilePath)) {
+      log.error(`No wproxy.js folder found in ${this.rootPath}`);
+      process.exit(-1);
     }
   }
-  else if (type === 'function') {
-    executerType = 'func';
+
+  _readConfigFileContent() {
+    this.config = requireUncached(this.configFilePath);
   }
-  return executerType;
-}
 
-function executeHttpResponder(rule, req, res) {
-  log.info(req.url, '----->', rule);
-  proxy.web(req, res, {
-    target: rule,
-    changeOrigin: true,
-  });
-}
-
-function executeDirResponder(mockPath, req, res) {
-  const url = parseUrl(req.url).pathname;
-  const jsonPath = path.join(rootPath, mockPath, `${url}.json`);
-  log.info(req.url, '----->', jsonPath);
-  const json = fs.readFileSync(jsonPath);
-  res.setHeader('Content-Type', 'application/json;charset=UTF-8');
-  res.end(json);
-}
-
-function executeFuncResponder(func, req, res) {
-  const json = JSON.stringify(func(req));
-  res.setHeader('Content-Type', 'application/json;charset=UTF-8');
-  res.end(json);
-}
-
-function executeRule(rule, req, res) {
-
-  // 判断类型 根据类型分别调用不同的方法
-  // 检查类型 分别给不同的路由函数进行处理
-  const executerType = getExecuterType(rule);
-  if (!executerType) {
-    log.error(`${rule} config error, please retry after fix it`)
-    process.exit(-1);
+  _getReplaceParamMap() {
+    let paramMap = {};
+    this.argvList.forEach((item) => {
+      const paramList = item.split('=');
+      if (paramList.length === 2) {
+        paramMap[paramList[0]] = paramList[1];
+      }
+    });
+    this.replaceVariableMap = paramMap;
   }
-  switch (executerType) {
-    case 'web':
-      executeHttpResponder(rule, req, res);
-      break;
-    case 'dir':
-      executeDirResponder(rule, req, res);
-      break;
-    case 'func':
-      executeFuncResponder(rule, req, res);
-      break;
+
+  _getCurrentEnvAndRuleList() {
+    const envList = Object.keys(this.config).filter((item) => item !== 'globalRules');
+    if (envList.length === 0) {
+      log.error(`No available env found in wproxy.js, please retry a later!!!`);
+      process.exit(-1);
+    }
+    let currentEnv = this._getCurrentEnv(envList);
+    if (!currentEnv) {
+      log.warn(`You have not set a  backend env params ,use ${envList[0]} as default env`);
+      currentEnv = envList[0];
+    }
+    this.currentRuleList = this.config[currentEnv];
   }
+
+  _getCurrentEnv(envs) {
+    let result = '';
+    this.argvList.forEach((item) => {
+      if (!item) return;
+      const key = item.split('=')[0];
+      if (envs.includes(key)) {
+        result = key;
+      }
+    });
+    return result;
+  }
+
+  _replaceVariableInResponder() {
+    const replaceKeys = Object.keys(this.replaceVariableMap);
+    this.currentRuleList = this.currentRuleList.map((rule) => {
+      if (getTypeOf(rule.responder) !== 'string') return rule;
+      replaceKeys.forEach((key) => {
+        const value = this.replaceVariableMap[key];
+        const regexp = `#\\{${key}\\}`;
+        rule.responder = rule.responder.replace(new RegExp(regexp, 'g'), value);
+      });
+      return rule;
+    });
+  }
+
+  _addGlobleRule() {
+    const globleRuleList = this.config.globalRules || [];
+    this.currentRuleList = globleRuleList.concat(this.currentRuleList);
+  }
+
+  getResponder(req) {
+    let currentRes = '';
+    this.currentRuleList.forEach((item) => {
+      if (item.pattern.test(req.url)) {
+        currentRes = item.responder;
+      }
+    });
+    return currentRes;
+  }
+
+  _getResponderType(responderProcessor) {
+    let responderType = '';
+    const type = getTypeOf(responderProcessor);
+    if (type === 'string') {
+      if (validUrl.isWebUri(responderProcessor)) {
+        responderType = 'web'; //  http or https
+      }
+      if (fs.existsSync(path.join(this.rootPath, responderProcessor))) {
+        responderType = 'dir'; //  local folder
+      }
+    }
+    else if (type === 'function') {
+      responderType = 'func';
+    }
+    return responderType;
+  }
+
+  _executeHttpResponder(rule, req, res) {
+    log.info(req.url, '----->', rule);
+    proxy.web(req, res, {
+      target: rule,
+      changeOrigin: true,
+    });
+  }
+
+  _executeDirResponder(mockPath, req, res) {
+    const url = parseUrl(req.url).pathname;
+    const jsonPath = path.join(this.rootPath, mockPath, `${url}.json`);
+    log.info(req.url, '----->', jsonPath);
+    const json = fs.readFileSync(jsonPath);
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    res.end(json);
+  }
+
+  _executeFuncResponder(func, req, res) {
+    const json = JSON.stringify(func(req));
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    res.end(json);
+  }
+
+  executeResponderProcessor(responderProcessor, req, res) {
+
+    // check type ,then dispatch corresponding processor
+    const responderType = this._getResponderType(responderProcessor);
+    if (!responderType) {
+      log.error(`${rule} config error, please retry after fix it`)
+      process.exit(-1);
+    }
+    switch (responderType) {
+      case 'web':
+        this._executeHttpResponder(responderProcessor, req, res);
+        break;
+      case 'dir':
+        this._executeDirResponder(responderProcessor, req, res);
+        break;
+      case 'func':
+        this._executeFuncResponder(responderProcessor, req, res);
+        break;
+    }
+  }
+
 }
 
-
+const beeProxy = new BeeProxy();
 module.exports = function (req, res, next) {
-
-  // 获取当前url适配的url,执行
-  const rule = getRule(currentRuleList, req, replaceParamMap);
-
-  if (rule) {
-    executeRule(rule, req, res);
+  const responderProcessor = beeProxy.getResponder(req);
+  if (responderProcessor) {
+    beeProxy.executeResponderProcessor(responderProcessor, req, res);
     return;
   }
   next && next();
